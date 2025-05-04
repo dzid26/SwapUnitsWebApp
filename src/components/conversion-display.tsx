627ac6b15592a2548e4c3f3745887985605af44b
Karol Bugaj <artificialfake@gmail.com>
2025-05-04 22:26:34 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.ol tabIndex=-1 className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[100] flex max-h-screen w-full flex-col p-4 md:max-w-[420px]">
    
    with the text content:
    
        Copied!Result "1,000 g" copied to clipboard.
    
    Relevant files:
    
        - https://6000-idx-studio-1746268909889.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev/_next/static/chunks/node_modules_f6275cf9._.js
        - https://6000-idx-studio-1746268909889.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev/_next/static/chunks/node_modules_f6275cf9._.js
        - https://6000-idx-studio-1746268909889.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev/_next/static/chunks/node_modules_f6275cf9._.js
        - https://6000-idx-studio-1746268909889.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev/_next/static/chunks/node_modules_f6275cf9._.js
        - https://6000-idx-studio-1746268909889.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev/_next/static/chunks/node_modules_f6275cf9._.js
        - https://6000-idx-studio-1746268909889.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev/_next/static/chunks/src_68d18129._.js
        - https://6000-idx-studio-1746268909889.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    disappear in 1.5 sec

--- src/components/conversion-display.tsx
+++ src/components/conversion-display.tsx
@@ -107,7 +107,7 @@ export const ConversionDisplay = React.memo(function ConversionDisplayComponent(
                 title: "Copied!",
                 description: `Result "${textToCopy}" copied to clipboard.`,
                 variant: "confirmation", // Use confirmation (green) variant
-                duration: 3000, // Set duration to 3 seconds (3000ms)
+                duration: 1500, // Set duration to 1.5 seconds (1500ms)
             });
         } catch (err) {
             console.error('Failed to copy text: ', err);
